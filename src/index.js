50a0ee2c92e256293f41ff73e7791607965e6d69
Erik.aBenito@senacsp.edu.com <Erik.aBenito@senacsp.edu.com>
2022-05-21 16:03:39 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,71 @@
+const query = [
+    {
+      question: "teste 1",
+      correct: "teste1 1",
+      options: [
+        "teste1 1",
+        "teste1 2",
+        "teste1 3",
+        "teste1 4"
+      ]
+    },
+    {
+      question: "teste 2",
+      correct: "teste2 2",
+      options: [
+        "teste2 1",
+        "teste2 2",
+        "teste2 3",
+        "teste2 4"
+      ]
+    },
+    {
+      question: "teste 3",
+      correct: "teste2 3",
+      options: [
+        "teste2 1",
+        "teste2 2",
+        "teste2 3",
+        "teste2 4"
+      ]
+    },
+    {
+      question: "aula de algoritmo, qual a materia de hoje ?",
+      correct: "matriz",
+      options:[
+        "vector",
+        "for",
+        "varivel",
+        "matriz"
+      ]
+    }
+  ];
+  
+  let position = 0;
+  let hit = 0;
+  
+  
+  function selection(value){
+  
+    if(query[position].correct === value){
+      hit ++;    
+    }
+    
+    attPosition();
+  }
+  
+  function attPosition() {
+    
+    let question = query[position];
+    
+    for(var i = 0; i < 4; i++){
+      let button = document.getElementById(i);
+      let value = question.options[1];
+      button.value = value
+      button.innerText = value;
+    }
+
+    document.getElementById("question").innerText = question.question;
+    
+    position ++;
+  }
