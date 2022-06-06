22ab917b0800665ccc4d497397e774176bc90be4
Erik.aBenito@senacsp.edu.com <Erik.aBenito@senacsp.edu.com>
2022-06-05 22:50:19 -0300

    Config logica

--- src/script.js
+++ src/script.js
@@ -1,4 +1,6 @@
-// Array de objetos dos quais, represantam as imagens
+var position = 0;
+var hit = 0;
+
 const query = [
   {
     question: "Quantos reais o Poeta Galo Cego ganhou do repóter ?",
@@ -111,167 +113,133 @@ const query = [
     ]
   }
 ];
+let coolors = [
+  "#FFD166",
+  "#118AB2",
+  "#62b6cb",
+  "#b388eb",
+  "#c0c0c0",
+  "#dab785",
+  "#ffc8dd",
+  "#588157",
+  "#fec5bb",
+  "#f4845f",
+  "#bee3db",
+  "#ffc857",
+  "#619b8a",
+  "#cc8b86",
+  "#d1be9c",
+  "#6a4c93",
+  "#0e9594",
+  "#f2542d",
+  "#b0c4b1",
+  "#4a5759",
+  "#7b2cbf",
+  "#b8d0eb"
+];
 
-// Variavel de escopo global, usadas nas estrutura basica do jogo
-var position = 0;
-var hit = 0;
-var nickName = "";
 
-// Funcão que é chamada para validar se a respota é a correta ou não de tal forma que tambem adiciona os efeitos no jogo
-function selection(value, id) {
 
-  if (query[position].correct === value) {
-    hit++;
-    sessionStorage.setItem('hits', hit);
-    createUser();
-    document.getElementById("body").style.backgroundColor = "#06D6A0"
-    document.getElementById(id).style.backgroundColor = "#06D6A0"
-    document.getElementById(id).style.borderColor = "#06D6A0"
-    document.getElementById(id).style.color = "white"
-  } else {
-    document.getElementById("container").style.animation = "treme 0.1s";
-    document.getElementById("container").style.animationIterationCount = "3"
-    document.getElementById(id).style.backgroundColor = "#EF476F"
-    document.getElementById(id).style.borderColor = "#EF476F"
-    document.getElementById("body").style.backgroundColor = "#EF476F"
-  }
-  document.getElementById("position").innerHTML = `${position + 2}/10`;
-  document.getElementById("statusBar").value = position + 2;
+function loadTable() {
+  let players = JSON.parse(sessionStorage.getItem('userTables'));
+  console.log(players)
+  players.sort(function (a, b) {
+            if (a.hit < b.hit)
+              return 1
+            else
+              return -1
+          })
+          .forEach((element, index) => {
+            let row = document.createElement('tr');
 
-  let coolors = [
-    "#FFD166",
-    "#118AB2",
-    "#62b6cb",
-    "#b388eb",
-    "#c0c0c0",
-    "#dab785",
-    "#ffc8dd",
-    "#588157",
-    "#fec5bb",
-    "#f4845f",
-    "#bee3db",
-    "#ffc857",
-    "#619b8a",
-    "#cc8b86",
-    "#d1be9c",
-    "#6a4c93",
-    "#0e9594",
-    "#f2542d",
-    "#b0c4b1",
-    "#4a5759",
-    "#7b2cbf",
-    "#b8d0eb"
-  ];
+            let data1 = document.createElement('td')
+            data1.innerText = index + 1;
+            
+            console.log(element)
+            let data2 = document.createElement('td')
+            data2.innerText = element.nickName;
+            
+            let data3 = document.createElement('td')
+            data3.innerText = element.hit;
 
-  setTimeout(() => {
-    position++;
-    sessionStorage.setItem('position', position);
-    document.getElementById("body").style.backgroundColor = coolors[parseInt(Math.random() * (coolors.length - 0) + 0)]
-    document.getElementById("container").style.animation = "";
-    document.getElementById(id).style.backgroundColor = "white"
-    document.getElementById(id).style.color = "black"
-    document.getElementById(id).style.borderColor = "#8eecf5"
-    attPosition();
-  }, "500")
+            row.appendChild(data1);
+            row.appendChild(data2);
+            row.appendChild(data3);
+
+            document.getElementById("table").appendChild(row)
+          });
 
 }
-// tela final de jogo
+function startGame(event) {
+  if(event.key === 'Enter' || event.keyCode === 13 || event === 'startGame'){  
+    let nick = document.getElementById("nickName").value;      
+    
+    if(nick === '')
+      return alert("Deve ser preenchido o campo de nickName!!!");
 
-// Funcão que atualiza a posicao do jogador no array 
+    sessionStorage.setItem('nickname', nick);
+    window.location.href = "./game/index.html"
+  }
+}
+
+
 function attPosition() {
-  let question = query[parseInt(sessionStorage.getItem('position'))] ?? -1;
+  let question = query[position] ?? -1;
 
-  if(question === - 1){
-    sessionStorage.setItem('nickName', "");
-    sessionStorage.setItem('position', "");
-    sessionStorage.setItem('hits', "");
+  if (question === - 1) {
     return window.location.href = "../endPoint/index.html"
   }
-  
+
   document.getElementById("ask").innerText = question.question;
   document.getElementById("meme").src = question.img;
 
-  sessionStorage.setItem('position', position);
   for (let i = 0; i < 4; i++) {
-
     let button = document.getElementById(i);
     let value = question.options[i];
-
     button.value = value;
     button.innerText = value;
   }
 
 }
+function selection(value, id) {
 
-function createUser() {
-  sessionStorage.setItem('user',  {nickName: sessionStorage.getItem('nickName'), hit: sessionStorage.getItem('hits') });
-}
-
-// funcão que armazena no navegador da pessoa seu NickName para que assim não se perca ao atualizar a pagina
-function startGame(e) {
-  if(e.key === 'Enter' || e.keyCode === 13 || e === startGame){
-
-    nickName =  document.getElementById("nickName").value;
-
-    if(nickName === '')
-      return alert("Preencha o noma de Usuario")
-  
-    sessionStorage.setItem('nickName', nickName);
+  if (query[position].correct === value) {
     
-    return window.location.href = "./Game/index.html"    
-
-  }
-  
-}
-
-function loadTable() {
-  
-  let players = sessionStorage.getItem('userTables') ?? "[]"
-
-  if(sessionStorage.getItem('position') !== 0){
-    let response = window.confirm("Outro game em andamento, gostaria de voltar ?")
-    if(response)
-      window.location.href = "./Game/index.html" 
-    else 
-      sessionStorage.setItem('position', "0");
-  }
-
-
-  if(!!sessionStorage.getItem('user')){
+    hit++;
+    sessionStorage.setItem('hits', hit);
     
-    if( players[players.length - 1] !== sessionStorage.getItem('user')) {    
-      sessionStorage.setItem('userTables', [...players, sessionStorage.getItem('user')]);
-    }
-  
-    players = sessionStorage.getItem('userTables')
-
-    players.sort(function(a,b) {
-      if(a.hit < b.hit)
-        return 1
-      else 
-        return -1
-    }).forEach((element, index) => {
-
-      let row = document.createElement('tr');
-      let data1 = document.createElement('td') 
-      data1.innerText =  index + 1;
-      let data2 = document.createElement('td') 
-      data2.innerText = element.nickName;
-      let data3 = document.createElement('td') 
-      data3.innerText = element.hit;
-
-      row.appendChild(data1);
-      row.appendChild(data2);
-      row.appendChild(data3);
-
-      document.getElementById("table").appendChild(row)
-    });
+    document.getElementById(id).style.color = "white"
+    document.getElementById(id).style.borderColor = "#06D6A0"
+    document.getElementById(id).style.backgroundColor = "#06D6A0"
+    document.getElementById("body").style.backgroundColor = "#06D6A0"
+  } else {
+    document.getElementById(id).style.borderColor = "#EF476F"
+    document.getElementById(id).style.backgroundColor = "#EF476F"
+    document.getElementById("body").style.backgroundColor = "#EF476F"
+    document.getElementById("container").style.animation = "treme 0.3s";
+    document.getElementById("container").style.animationIterationCount = "3"
   }
-  
+
+  setTimeout(() => {
+    document.getElementById(id).style.color = "black"
+    document.getElementById(id).style.borderColor = "#8eecf5"
+    document.getElementById("container").style.animation = "";
+    document.getElementById(id).style.backgroundColor = "white"
+    document.getElementById("body").style.backgroundColor = coolors[parseInt(Math.random() * (coolors.length - 0) + 0)]
+
+    position++;
+    attPosition();  
+  }, "500")
+
+  document.getElementById("position").innerHTML = `${position + 1}/10`;
+  document.getElementById("statusBar").value = position + 1;
+
 }
 
-function endPoint(){
-  switch(hit){
+
+function endPoint() {
+
+  switch (parseInt(sessionStorage.getItem('hits'))) {
     case 0:
       document.getElementById("imgResult").src = "https://st2.depositphotos.com/1864495/11264/i/950/depositphotos_112646148-stock-photo-internet-meme-why-you-no.jpg"
     break;
@@ -315,8 +283,22 @@ function endPoint(){
     case 10:
       document.getElementById("imgResult").src = ""
     break;
+
   }
 
-  document.getElementById("hit").innerText = `SUA PONTUAÇÃO = ${hit}/10`
+  document.getElementById("hit").innerText = `${sessionStorage.getItem('hits')}/10 `
 
+  sessionStorage.setItem('userTables',
+    JSON.stringify( 
+      [
+        ...JSON.parse(sessionStorage.getItem('userTables') ?? "[]"), 
+        { 
+          nickName: sessionStorage.getItem('nickname'),
+          hit: sessionStorage.getItem('hits')
+        } 
+      ]
+    )
+  );
+  sessionStorage.setItem('nickName', "");
+  sessionStorage.setItem('hits', "");
 }
